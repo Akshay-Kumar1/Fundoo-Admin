7d09f6f0e3c676ec5848235f58b8d53b56119dfa
Akshay-Kumar1 <akshayk24396@gmail.com>
2018-10-17 19:54:57 +0530

    commit

--- src/app/component/admin-dashboard/admin-dashboard.component.ts
+++ src/app/component/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+import 'datatables.net';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -10,6 +11,70 @@ export class AdminDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+    $(document).ready(function()
+    {
+      
+    
+    $(function(){
+       $.ajax({
+            type: 'GET',
+            url:'http://34.213.106.173/api/user/getAdminUserList',
+            success:function(result)
+            {
+              // console.log(result)
+              var array=[];
+              for(var i=0;i<result.data.data.length;i++)
+              {
+                  array.push([i+1,result.data.data[i].firstName,result.data.data[i].lastName,result.data.data[i].email,result.data.data[i].service])
+              }
+              (<any>$('#tableid')).DataTable({
+                data:array,
+              });
+            },
+            error:function(error)
+            {
+              console.log(`Error ${error}`)
+            }
+        })
+        return false;
+      })
+      })
+      $(document).ready(function()
+      {
+        var token=localStorage.getItem('token');
+  
+      $(function(){
+         $.ajax({
+          
+              type: 'GET',
+              url:'http://34.213.106.173/api/user/UserStatics',
+              headers:
+              {
+                'Authorization':token
+              },
+              success:function(result)
+              {
+               var arr=result.data.details;
+               var html='';
+               for(let index=0;index<arr.length;index++)
+               {
+                 html+="<span class='card'>";
+                 html+="<span class='card-header'>"+arr[index].service+"</span>";
+                 html+="<span class='card-body'>"+arr[index].count+"</span>";
+                 html+="<span";
+                 $("#services").html(html);
+               }
+
+              },
+              error:function(error)
+              {
+                console.log(`Error ${error}`)
+              }
+          })
+          return false;
+        })
+      })
   }
 
 }
--- src/app/component/admin-login/admin-login.component.ts
+++ src/app/component/admin-login/admin-login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { post } from 'selenium-webdriver/http';
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
@@ -9,13 +10,40 @@ export class AdminLoginComponent implements OnInit {
 
   constructor() { }
 
+ 
   ngOnInit() {
-    $(document).ready(function(){
-      $("button").click(function(){
-          var div = $("div");  
-          div.animate({right: '5em'}, "fast");
-          div.animate({left: '5em'}, "fast");
-      });
-  });
+
+$(document).ready(function()
+{
+  
+const Url='http://34.213.106.173/api/user/adminLogin'
+$('#login').click(function(){
+
+
+const admin= {
+       "email": $("#inputEmail").val(),
+       "password": $("#inputPassword").val()
+   };
+ 
+   $.ajax({
+        type: 'POST',
+        url:Url ,
+        data:admin,
+        success:function(result)
+        {
+          localStorage.setItem('token',result.id);
+          console.log(result)
+          window.location.href='/adminDashboard'
+        },
+        error:function(error)
+        {
+          console.log(`Error ${error}`)
+          $(`p#error`).html('Email/Password Incorrect')
+        }
+    })
+    return false;
+  })
+  }) 
 }
+
 }
